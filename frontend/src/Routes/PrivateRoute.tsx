e46c448d6992137dbaff364b1bb9f73997f50ec2
diogg0 <ratnakirti1234@gmail.com>
2025-03-23 11:03:01 +0530

    Fixed Registration page and

--- frontend/src/Routes/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const token = localStorage.getItem("jwt"); 
-
-  return token ? <Outlet /> : <Navigate to="/auth" replace />;
-};
-
-export default PrivateRoute;
